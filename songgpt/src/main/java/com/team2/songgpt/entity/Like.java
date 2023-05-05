dc7a1e46b44d61b655556a758b2f6fb201043b8d
DESKTOP-QICCFCP\mdc <mdc53@naver.com>
2023-05-05 20:07:47 +0900

    feat : 좋아요 기능

---
+++ songgpt/src/main/java/com/team2/songgpt/entity/Like.java
@@ -0,0 +1,30 @@
+package com.team2.songgpt.entity;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+public class Like {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    public PostLike(Member member, Post post) {
+        this.post = post;
+        this.member = member;
+    }
+}

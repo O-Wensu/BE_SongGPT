907417d3923ac68504eb43b2571054cee725b887
Hohomii <glgltjsal@naver.com>
2023-05-06 11:38:57 +0900

    Feat : AccessToken, RefreshToken 기능 추가

---
+++ songgpt/src/main/java/com/team2/songgpt/dto/member/TokenDto.java
@@ -0,0 +1,15 @@
+package com.team2.songgpt.dto.member;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter @NoArgsConstructor
+public class TokenDto {
+    private String accessToken;
+    private String refreshToken;
+
+    public TokenDto(String accessToken, String refreshToken) {
+        this.accessToken = accessToken;
+        this.refreshToken = refreshToken;
+    }
+}
--- songgpt/src/main/java/com/team2/songgpt/global/jwt/JwtAuthenticationFilter.java
+++ songgpt/src/main/java/com/team2/songgpt/global/jwt/JwtAuthenticationFilter.java
@@ -1,5 +1,9 @@
 package com.team2.songgpt.global.jwt;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.team2.songgpt.entity.Member;
+import com.team2.songgpt.global.dto.ResponseDto;
+import com.team2.songgpt.repository.MemberRepository;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -9,6 +13,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
@@ -23,17 +28,35 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
                                     HttpServletResponse response,
                                     FilterChain filterChain) throws ServletException, IOException {
         // 헤더의 토큰 가져오기
-        String access_token = jwtUtil.resolveToken(request);
+        String access_token = jwtUtil.resolveToken(request, JwtUtil.ACCESS_TOKEN);
+        String refresh_token = jwtUtil.resolveToken(request, JwtUtil.REFRESH_TOKEN);
 
-        if (access_token != null && jwtUtil.validateToken(access_token))
-            setAuthentication(jwtUtil.getUserInfoFromToken(access_token));
-
+        if (access_token != null) {
+            if (jwtUtil.validateToken(access_token)) {
+                setAuthentication(jwtUtil.getUserInfoFromToken(access_token));
+            }
+            else if (refresh_token != null) {
+                boolean isRefreshToken = jwtUtil.refreshTokenValidation(refresh_token);
+                if (!isRefreshToken) {
+                    String email = jwtUtil.getUserInfoFromToken(refresh_token);
+                    String newAccessToken = jwtUtil.createToken(email, JwtUtil.ACCESS_TOKEN);
+                    jwtUtil.setHeaderAccessToken(response, newAccessToken);
+                    setAuthentication(jwtUtil.getUserInfoFromToken(newAccessToken));
+                } else {
+                    response.setContentType("application/json; charset=utf8");
+                    ResponseDto responseDto = ResponseDto.setBadRequest("토큰을 찾을 수 없습니다.");
+                    String json = new ObjectMapper().writeValueAsString(responseDto);
+                    response.getWriter().write(json);
+                    return;
+                }
+            }
+        }
         filterChain.doFilter(request, response);
     }
 
-    public void setAuthentication(String userId) {
+    public void setAuthentication(String email) {
         SecurityContext context = SecurityContextHolder.createEmptyContext();
-        Authentication authentication = jwtUtil.createAuthentication(userId);
+        Authentication authentication = jwtUtil.createAuthentication(email);
         context.setAuthentication(authentication);
 
         SecurityContextHolder.setContext(context);

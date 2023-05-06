1987e30230eee8bf7aeeedce727d1e953057300a
rnjswo9578 <rnjswo153486@gmail.com>
2023-05-06 10:38:03 +0900

    update ChatGpt 230506

--- songgpt/src/main/java/com/team2/songgpt/controller/PostController.java
+++ songgpt/src/main/java/com/team2/songgpt/controller/PostController.java
@@ -16,7 +16,6 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-
 @RequiredArgsConstructor
 public class PostController {
 
--- songgpt/src/main/java/com/team2/songgpt/dto/gpt/GptResponseDto.java
+++ songgpt/src/main/java/com/team2/songgpt/dto/gpt/GptResponseDto.java
@@ -1,5 +1,31 @@
 package com.team2.songgpt.dto.gpt;
 
-// temp
-public class GptResponseDto {
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
+public class GptResponseDto implements Serializable {
+    private String id;
+    private String object;
+    private LocalDate created;
+    private String model;
+    private List<Choice> choices;
+
+    @Builder
+    public GptResponseDto(
+            String id, String object,
+            LocalDate created, String model,
+            List<Choice> choices) {
+        this.id = id;
+        this.object = object;
+        this.created = created;
+        this.model = model;
+        this.choices = choices;
+    }
 }

b1a4d871fc4e78f4887e3497e730b3911e2d1fc5
sojk401 <sojk401@gmail.com>
2023-05-09 19:09:00 +0900

    Chore: cors 허용 url, 메서드 추가

--- songgpt/src/main/java/com/team2/songgpt/global/config/WebConfig.java
+++ songgpt/src/main/java/com/team2/songgpt/global/config/WebConfig.java
@@ -37,6 +37,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000", "http://whats-your-favorite-music.s3-website.ap-northeast-2.amazonaws.com")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .exposedHeaders("Access_Token");
     }
 }

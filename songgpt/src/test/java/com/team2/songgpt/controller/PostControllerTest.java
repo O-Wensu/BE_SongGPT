ba46c7949c2b3f95f0c0ba37e60e804334c02a1e
sojk401 <sojk401@gmail.com>
2023-05-10 18:47:36 +0900

    Test: 게시글 등록/삭제/삭제시 에러 테스트 케이스 작성

--- songgpt/src/test/java/com/team2/songgpt/controller/PostControllerTest.java
+++ songgpt/src/test/java/com/team2/songgpt/controller/PostControllerTest.java
@@ -1,24 +1,102 @@
 package com.team2.songgpt.controller;
 
+import com.team2.songgpt.dto.post.PostRequestDto;
+import com.team2.songgpt.entity.Member;
+import com.team2.songgpt.global.dto.ResponseDto;
+import com.team2.songgpt.global.entity.FeelEnum;
+import com.team2.songgpt.global.entity.GenreEnum;
+import com.team2.songgpt.global.entity.WeatherEnum;
+import com.team2.songgpt.repository.MemberRepository;
+import com.team2.songgpt.service.PostService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
+@Rollback
+@Transactional
 class PostControllerTest {
 
+    @Autowired
+    private PostService postService;
+    @Autowired
+    private MemberRepository memberRepository;
+
     @Test
-    void getPosts() {
+    @DisplayName("게시글 등록")
+    void testSavePost() {
+        //given
+        Member member = registerMember("user111@naver.com", "user111!!", "user111");
+        PostRequestDto postRequestDto = makePostRequestDto();
+
+        //when
+        ResponseDto<Long> responseDto = postService.savePost(postRequestDto, member);
+
+        //then
+        Assertions.assertThat(responseDto.getData()).isEqualTo(1L);
     }
 
     @Test
-    void getPost() {
+    @DisplayName("게시글 삭제")
+    void testDeletePost() {
+        //given
+        Member member = registerMember("user111@naver.com", "user111!!", "user111");
+        PostRequestDto postRequestDto = makePostRequestDto();
+        Long savedId = postService.savePost(postRequestDto, member).getData();
+
+        //when, then
+        assertDoesNotThrow(() -> {
+            postService.deletePost(savedId, member);
+        });
     }
 
     @Test
-    void savePost() {
+    @DisplayName("회원 정보와 게시글 작성자가 다르면 에러가 발생한다")
+    void testDeletePostNoAuthorizeError() {
+        //given
+        Member member = registerMember("user111@naver.com", "user111!!", "user111");
+        PostRequestDto postRequestDto = makePostRequestDto();
+        Long savedId = postService.savePost(postRequestDto, member).getData();
+
+        Member member2 = registerMember("user222@naver.com", "user222!!", "user222");
+
+        //when, then
+        assertThrows(IllegalArgumentException.class,
+                () -> postService.deletePost(savedId, member2));
     }
 
     @Test
-    void deletePost() {
+    @DisplayName("삭제하려는 게시글이 없으면 에러가 발생한다")
+    void testDeletePostNoExistPostError() {
+        //given
+        Member member = registerMember("user111@naver.com", "user111!!", "user111");
+
+        //when, then
+        assertThrows(IllegalArgumentException.class,
+                () -> postService.deletePost(1L, member));
+    }
+
+    @Test
+    private PostRequestDto makePostRequestDto() {
+        return PostRequestDto.builder()
+                .question("질문 내용")
+                .answer("답변 내용")
+                .feelTag(FeelEnum.SAD)
+                .weatherTag(WeatherEnum.CLOUD)
+                .genreTag(GenreEnum.HIPHOP)
+                .gradient("#FFFFFF")
+                .build();
+    }
+
+    private Member registerMember(String email, String password, String nickname) {
+        Member member = new Member(email, password, nickname);
+        memberRepository.save(member);
+        return member;
     }
 }
